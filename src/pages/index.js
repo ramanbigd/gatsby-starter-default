8ff1c3e3e041feb726e150e314ff3b48a512b720
ramanprasad <95390875+ramanbigd@users.noreply.github.com>
2021-12-02 21:47:14 +1300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,39 @@ const IndexPage = () => (
     <h1>This is a real website for raman prasad.</h1>
      <h1>I didn't capatlise my name becuase small letters look asthetically more pleasing.</h1>
      <h>My Links</h>
+<!-- Display the countdown timer in an element -->
+<p id="demo"></p>
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
     <p>These are my socials down below.</p>
     <p>I also make music.</p>
     <StaticImage
